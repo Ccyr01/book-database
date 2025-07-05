3f2e84378894236a63b287a58b27e3e4f6358c0c
Christian Cyr <ccyr01@nmsu.edu>
2025-07-05 00:13:07 -0600

    added our database model

--- models.py
+++ models.py
@@ -1,8 +1,25 @@
 #create a database
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 #books.db
+engine = create_engine('sqlite:///books.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
 
 #create a model
+class Book(Base):
+    __tablename__ = 'books'
 
-#title, author, date published, price
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String)
+    author = Column("Author", String)
+    published_date = Column('Published', Date)
+    price = Column('Price', Integer)
+
+    def __repr__(self):
+        return f'Title: {self.title} Author: {self.author} Published: {self.published_date} Price: {self.price}'
 
